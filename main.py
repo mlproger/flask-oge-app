ffe791019ddb5dba851003adb4cd29ab1b0c01fb
mlproger <fed4enko2018@yandex.ru>
2022-12-05 00:24:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5000), threaded=True)
