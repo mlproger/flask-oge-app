ba428079eb9b0e6a216a174ff11eb254553bf811
mlproger <fed4enko2018@yandex.ru>
2022-12-04 21:30:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from flask_restful import Api, Resource, reqparse
 import os
 import text_sum
 
-
 class CRUDClass(Resource):
     def get(self, correct, user_answer):
         return text_sum.getResult(user_answer, correct)
 
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(CRUDClass, "/<string:correct>/<string:user_answer>")
+app.run(debug=True, port=os.getenv("PORT", default=5000))
 
-if __name__ == __name__:
-    app = Flask(__name__)
-    api = Api(app)
-    api.add_resource(CRUDClass, "/<string:correct>/<string:user_answer>")
+
+
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+
+if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
