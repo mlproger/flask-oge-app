ba4a98c413252a8ebceb8074bdb8ece14cd212e3
mlproger <fed4enko2018@yandex.ru>
2022-12-06 20:18:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,60 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource, reqparse
 import os
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-
+import text_sum
+from celery import Celery
 import spacy
-from spacy.lang.ru.examples import sentences 
+
+def getResult(f_s, s_s):
+    nlp = spacy.load("ru_core_news_lg")
+    f_s = nlp(f_s)
+    s_s = nlp(s_s)
+    return f_s.similarity(s_s)
+
+
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config["CELERY_RESULT_BACKEND"],
+        broker=app.config["CELERY_BROKER_URL"],
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
 
 
 app = Flask(__name__)
-api = Api(app)
-limiter = Limiter(
-    app,
-    key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"],
-    storage_uri="memory://",
+app.config.update(
+    CELERY_BROKER_URL="/http://localhost:5000/",
+    CELERY_RESULT_BACKEND="/http://localhost:5000/",
 )
+api = Api(app)
+celery = make_celery(app)
 
+@celery.task()
+def rec(correct, user_answer):
+    return text_sum.getResult(user_answer, correct)
 
 
 @app.route('/')
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+
 @app.route('/<string:correct>/<string:user_answer>')
-def rec(correct, user_answer):
-    nlp = spacy.load("ru_core_news_lg")
-    user_answer = nlp(user_answer)
-    correct = nlp(correct)
-    ans = user_answer.similarity(correct)
-    return jsonify({"ans": ans})
+def ind(correct, user_answer):
+    results = rec(correct, user_answer)
+    return jsonify(results)
+    
+    
+     
 
 @app.route('/test')
 def hello():
