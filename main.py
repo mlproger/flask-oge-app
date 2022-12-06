e366bc9f0b98c76ca3d5252523e166424ca9ac66
mlproger <fed4enko2018@yandex.ru>
2022-12-07 00:32:37 +0300

    1

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ import text_sum
 from flask import Flask, jsonify
 from flask_restful import Api, Resource, reqparse
 import os
-
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
 api = Api(app)
 
+limiter = Limiter(
+    app,
+    default_limits=["200 per day", "50 per hour"],
+    key_func=get_remote_address,
+    #storage_uri="https://localhost:5000",
+)
+
 
 
 @app.route('/<string:correct>/<string:user_answer>')
+@limiter.limit("2/seconds")
 async def ind(correct, user_answer):
     return jsonify({"A": text_sum.getResult(user_answer, correct)})
     
-@app.route('/')
-def hi():
-    return jsonify({"Ans": "HI"})   
+    
     
 @app.route('/test')
 def hello():
--- text_sum.py
+++ text_sum.py
@@ -1,15 +1,27 @@
+import asyncio
+import spacy
+import difflib
 from sentence_transformers import SentenceTransformer, util
+import numpy as np
 
 
 def getResult(f_s, s_s):
-    model = SentenceTransformer('symanto/sn-xlm-roberta-base-snli-mnli-anli-xnli')
-    embedding1 = model.encode(f_s, convert_to_tensor=True)
-    embedding2 = model.encode(s_s, convert_to_tensor=True)
-    cosine_scores = util.pytorch_cos_sim(embedding1, embedding2)
-    print(float(cosine_scores))
-    return float(cosine_scores)
+    print(1)
+    nlp = spacy.load("ru_core_news_lg")
+    print(2)
+    f_s = nlp(f_s)
+    s_s = nlp(s_s)
+    return f_s.similarity(s_s)
+    
 
 
 
 
 
+
+# model = SentenceTransformer('symanto/sn-xlm-roberta-base-snli-mnli-anli-xnli')
+#     embedding1 = model.encode(f_s, convert_to_tensor=True)
+#     embedding2 = model.encode(s_s, convert_to_tensor=True)
+#     cosine_scores = util.pytorch_cos_sim(embedding1, embedding2)
+#     print(float(cosine_scores))
+#     return float(cosine_scores)
