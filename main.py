7e45bfd89affa1e9579dd2d55ac55642a670b791
mlproger <fed4enko2018@yandex.ru>
2022-12-06 23:21:05 +0300

    d

--- main.py
+++ main.py
@@ -1,65 +1,25 @@
+import text_sum
 from flask import Flask, jsonify
 from flask_restful import Api, Resource, reqparse
 import os
-import text_sum
-from celery import Celery
-import spacy
-
-def getResult(f_s, s_s):
-    nlp = spacy.load("ru_core_news_lg")
-    f_s = nlp(f_s)
-    s_s = nlp(s_s)
-    return f_s.similarity(s_s)
-
-
-def make_celery(app):
-    celery = Celery(
-        app.import_name,
-        backend=app.config["CELERY_RESULT_BACKEND"],
-        broker=app.config["CELERY_BROKER_URL"],
-    )
-    celery.conf.update(app.config)
-
-    class ContextTask(celery.Task):
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return self.run(*args, **kwargs)
-
-    celery.Task = ContextTask
-    return celery
-
 
 
 app = Flask(__name__)
-app.config.update(
-    CELERY_BROKER_URL="/http://localhost:5000/",
-    CELERY_RESULT_BACKEND="/http://localhost:5000/",
-)
 api = Api(app)
-celery = make_celery(app)
 
-@celery.task()
-def rec(correct, user_answer):
-    return text_sum.getResult(user_answer, correct)
-
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 
 @app.route('/<string:correct>/<string:user_answer>')
-def ind(correct, user_answer):
-    results = rec(correct, user_answer)
-    return jsonify(results)
+async def ind(correct, user_answer):
+    return jsonify({"A": text_sum.getResult(user_answer, correct)})
     
+@app.route('/')
+def hello():
+    return jsonify({"Ans": "HI"})   
     
-     
-
 @app.route('/test')
 def hello():
     return jsonify({"Ans": "Test"})
-
 
 
 
--- text_sum.py
+++ text_sum.py
@@ -1,13 +1,13 @@
-import spacy
-from spacy.lang.ru.examples import sentences 
+from sentence_transformers import SentenceTransformer, util
+
 
 def getResult(f_s, s_s):
-    nlp = spacy.load("ru_core_news_lg")
-    f_s = nlp(f_s)
-    s_s = nlp(s_s)
-
-    return f_s.similarity(s_s)
-
+    model = SentenceTransformer('symanto/sn-xlm-roberta-base-snli-mnli-anli-xnli')
+    embedding1 = model.encode(f_s, convert_to_tensor=True)
+    embedding2 = model.encode(s_s, convert_to_tensor=True)
+    cosine_scores = util.pytorch_cos_sim(embedding1, embedding2)
+    print(float(cosine_scores))
+    return float(cosine_scores)
 
 
 
